fd304a95047960668d926e4b05942adb51079aa6
Changyu Li <Changyu.Li@microsoft.com>
2024-07-11 18:07:09 -0700

    Show more excerpt in jump history and add pinning

--- src/jumphistory.ts
+++ src/jumphistory.ts
@@ -15,6 +15,7 @@ class FilePosition {
 
 export class JumpHistoryTreeDataProvider implements vscode.TreeDataProvider<JumpHistoryTreeItem> {
     private jumps: Array<JumpHistoryTreeItem> = [];
+    private pinned: Array<PinnedJumpHistoryTreeItem> = [];
     public currentPosition?: FilePosition;
     private MAX_JUMP_HISTORY = 99;
 
@@ -29,7 +30,7 @@ export class JumpHistoryTreeDataProvider implements vscode.TreeDataProvider<Jump
     }
 
     getChildren(element?: JumpHistoryTreeItem): Thenable<JumpHistoryTreeItem[]> {
-        return Promise.resolve(this.jumps);
+        return Promise.resolve((this.pinned as JumpHistoryTreeItem[]).concat(this.jumps));
     }
 
     refresh(): void {
@@ -51,20 +52,36 @@ export class JumpHistoryTreeDataProvider implements vscode.TreeDataProvider<Jump
     setPosition(file: vscode.Uri, position: vscode.Position) {
         this.currentPosition = new FilePosition(file, position);
     }
+
+    pin(item: JumpHistoryTreeItem) {
+        let alreadyPinnedIndex = this.pinned.findIndex((p) => p.originalUnpinnedId === item.id);
+        if (alreadyPinnedIndex === -1) {
+            this.pinned.push(new PinnedJumpHistoryTreeItem(item.file, item.position, item.lineText, item.excerpt, item.id!));
+        } else {
+            this.pinned.splice(alreadyPinnedIndex, 1);
+        }
+        this._onDidChangeTreeData.fire();
+    }
+
+    unpin(item: PinnedJumpHistoryTreeItem) {
+        this.pinned.splice(this.pinned.indexOf(item), 1);
+        this._onDidChangeTreeData.fire();
+    }
 }
 
 export class JumpHistoryTreeItem extends vscode.TreeItem {
     constructor(
         public file: vscode.Uri,
         public position: vscode.Position,
+        public lineText: string,
         public excerpt: string
     ) {
         const id = uuidv4();
         super(id);
-        this.label = path.basename(file.path);
+        this.label = `${path.basename(file.path)}:${position.line + 1}:${position.character + 1}`;
         this.collapsibleState = vscode.TreeItemCollapsibleState.None;
         this.id = id;
-        this.description = `${position.line + 1}:${position.character + 1}`;
+        this.description = lineText;
         this.tooltip = excerpt;
 
         this.command = <vscode.Command>{
@@ -82,16 +99,44 @@ export class JumpHistoryTreeItem extends vscode.TreeItem {
     contextValue = 'JumpHistoryTreeItem';
 }
 
+export class PinnedJumpHistoryTreeItem extends JumpHistoryTreeItem {
+    constructor(
+        public file: vscode.Uri,
+        public position: vscode.Position,
+        public lineText: string,
+        public excerpt: string,
+        public originalUnpinnedId: string,
+    ) {
+        super(file, position, lineText, excerpt);
+    }
+
+    iconPath = new vscode.ThemeIcon("timeline-pin");
+
+    contextValue = 'PinnedJumpHistoryTreeItem';
+}
+
+
 export function initializeJumpHistory(context: vscode.ExtensionContext) {
     jumpHistory = new JumpHistoryTreeDataProvider();
     vscode.window.registerTreeDataProvider(
 		'jumpHistoryView',
 		jumpHistory
 	);
+    context.subscriptions.push(vscode.commands.registerCommand('call-graph-maker.jumpHistoryView.pin', async (item: JumpHistoryTreeItem) => {
+		jumpHistory.pin(item);
+	}));
+
+    context.subscriptions.push(vscode.commands.registerCommand('call-graph-maker.jumpHistoryView.unpin', async (item: PinnedJumpHistoryTreeItem) => {
+		jumpHistory.unpin(item);
+	}));
 }
 
 export function updatePosition(event: vscode.TextEditorSelectionChangeEvent): void {
     jumpHistory.setPosition(event.textEditor.document.uri, event.selections[0].anchor);
+}
+
+function clamp(num: number, min: number, max: number) {
+    return Math.min(Math.max(num, min), max);
 }
 
 export function notifyNavigation(event: vscode.TextEditorSelectionChangeEvent): void {
@@ -121,7 +166,17 @@ export function notifyNavigation(event: vscode.TextEditorSelectionChangeEvent): 
             lineEndOffset = 0;
         }
     }
-    const excerpt = text.slice(lineStartOffset, lineEndOffset);
+    const lineText = text.slice(lineStartOffset, lineEndOffset).trim();
 
-    jumpHistory.add(new JumpHistoryTreeItem(event.textEditor.document.uri, event.selections[0].anchor, excerpt));
+    const EXCERPT_CONTEXT = 10;
+    const excerptLineStart = clamp(event.selections[0].anchor.line - EXCERPT_CONTEXT, 0, event.textEditor.document.lineCount - 1);
+    const excerptLineEnd = clamp(event.selections[0].anchor.line + EXCERPT_CONTEXT, 0, event.textEditor.document.lineCount - 1);
+    lineEndOffset = text.indexOf('\n', event.textEditor.document.offsetAt(new vscode.Position(excerptLineEnd, 0)));
+    if (lineEndOffset === -1) {
+        lineEndOffset = clamp(text.length - 1, 0, text.length);
+    }
+    const excerpt = text.slice(event.textEditor.document.offsetAt(new vscode.Position(excerptLineStart, 0)),
+        lineEndOffset);
+
+    jumpHistory.add(new JumpHistoryTreeItem(event.textEditor.document.uri, event.selections[0].anchor, lineText, excerpt));
 }
